f088d7900ba0ced867afcd89f0c5c1ad32127ffa
Josh Chiu <joshc.work.inbox@gmail.com>
2026-01-03 18:43:32 -0500

    bugfix

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 // store.js
-import { configureStore } from '@reduxjs/toolkit';
-import venueReducer from './venueSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import venueReducer from "./venueSlice";
+import avReducer from "./avSlice";
 
 export default configureStore({
   reducer: {
-    // global react state venue is mapped to 
+    // global react state venue is mapped to
     venue: venueReducer,
     av: avReducer,
   },
